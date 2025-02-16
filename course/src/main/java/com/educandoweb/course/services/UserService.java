556bd72b3ea08dac96be01fc946c974096421bfa
lucas <lucascarlosrodex@gmail.com>
2025-02-15 21:17:16 -0300

    User update

--- course/src/main/java/com/educandoweb/course/services/UserService.java
+++ course/src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,9 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
-
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
@@ -35,6 +33,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 
 
 }
